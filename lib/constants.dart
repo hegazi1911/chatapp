0d3f43f062f2980eed21ffbb7fcbea0e199e5552
hegazi1911 <ahmedhegazi26@gmail.com>
2023-11-07 22:57:33 +0200

    Edite chat Page

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromRGBO(255, 88, 88, 1);
+const kLogo = 'assets/images/logo.png';
+const kMessagesCollections = 'messages';
+const kMessage = 'message';
+const kCreatedAt = 'createdAt'; 
